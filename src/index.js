e15cb6faa3ebe4ef0188eeefb433735eb961df74
pengziqian <pengziqian@zhihu.com>
2023-10-30 16:16:54 +0800

    chore: npm 安装依赖&显示引入依赖&webpack 打包&引用打包后的 js 文件&移动 index.html 文件

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
+import { join } from "lodash"
+
 function component() {
   const element = document.createElement("div")
 
   // lodash（目前通过一个 script 脚本引入）对于执行这一行是必需的
-  element.innerHTML = _.join(["Hello", "webpack"], " ")
+  element.innerHTML = join(["Hello", "webpack"], " ")
 
   return element
 }
