466c20b3539ac81d3ae1803d9794549207170013
pengziqian <pengziqian@zhihu.com>
2023-11-01 16:11:36 +0800

    chore: 回退处理

--- src/index.js
+++ src/index.js
@@ -1,19 +1,19 @@
 import { join } from "lodash"
-import iconUrl from "./icon.png"
+// import iconUrl from "./icon.png"
 // 样式定义打包后被放在了 style 标签中
-import "./style.css"
+// import "./style.css"
 
 function component() {
   const element = document.createElement("div")
+  // lodash（目前通过一个 script 脚本引入）对于执行这一行是必需的
   element.innerHTML = join(["Hello", "webpack"], " ")
   // 插入图片
-  const icon = new Image()
-  icon.src = iconUrl
-  icon.alt = ""
-  icon.width = 100
-  element.appendChild(icon)
-  // lodash（目前通过一个 script 脚本引入）对于执行这一行是必需的
-  element.classList.add("red")
+  // const icon = new Image()
+  // icon.src = iconUrl
+  // icon.alt = ""
+  // icon.width = 100
+  // element.appendChild(icon)
+  // element.classList.add("red")
 
   return element
 }
--- webpack.config.js
+++ webpack.config.js
@@ -9,16 +9,16 @@ module.exports = {
     filename: "bundle.js",
     path: path.resolve(__dirname, "dist"),
   },
-  module: {
-    rules: [
-      {
-        test: /\.css$/i, // 处理 css 文件，先安装依赖，再配置
-        use: ["style-loader", "css-loader"], // 逆序执行
-      },
-      {
-        test: /\.(png|svg|jpg|jpeg|gif)$/i, // 处理 css 文件，先安装依赖，再配置
-        type: "asset/resource",
-      },
-    ],
-  },
+  // module: {
+  //   rules: [
+  //     {
+  //       test: /\.css$/i, // 处理 css 文件，先安装依赖，再配置
+  //       use: ["style-loader", "css-loader"], // 逆序执行
+  //     },
+  //     {
+  //       test: /\.(png|svg|jpg|jpeg|gif)$/i, // 处理 css 文件，先安装依赖，再配置
+  //       type: "asset/resource",
+  //     },
+  //   ],
+  // },
 }
