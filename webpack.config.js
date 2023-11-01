1a4224081b1cd8c52f040c5d48514d3da11ac9a3
pengziqian <pengziqian@zhihu.com>
2023-11-01 16:36:43 +0800

    chore: 动态生成 index.html

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,8 @@
 const path = require("path")
+// 所以这个插件具体做了哪些事情？
+// 1. 动态生成 index.html（感觉主要是动态引入生成的 bundle？）
+// 2. 替换 output 目录中已存在的 index.html
+const HtmlWebpackPlugin = require("html-webpack-plugin")
 
 // 这里有个细节：平常使用的模块语法（引入、导出）是 es6 语法；这里用到的是 CommonJS
 module.exports = {
@@ -9,6 +13,7 @@ module.exports = {
     filename: "[name].bundle.js", // 动态生成 bundle 名称
     path: path.resolve(__dirname, "dist"),
   },
+  plugins: [new HtmlWebpackPlugin({ title: "管理输出" })],
   // module: {
   //   rules: [
   //     {
