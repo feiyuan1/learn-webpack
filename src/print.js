f63078cab30b31b322a63da3e16a8e5b4430bcfd
pengziqian <pengziqian@zhihu.com>
2023-11-09 11:02:50 +0800

    chore: 尝试一个输出&indexjs 中包含其他文件的引用

--- src/print.js
+++ src/print.js
@@ -1,3 +1,8 @@
+// 重复引用同一个依赖 lodash
+import _ from "lodash"
+
+console.log(_.join(["Another", "module", "loaded!"], " "))
+
 export default function print() {
   console.log("I get called from print.js")
 }
