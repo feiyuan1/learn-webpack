c954d7823aa8d7844fb02e3cd7059249dc272f13
feiyuan peng <feiyuan2987@163.com>
2025-02-17 10:14:09 +0800

    chore: try react scheduler

--- esModule/src/memory/schedule.js
+++ esModule/src/memory/schedule.js
@@ -1,13 +1,13 @@
 /**
  * yieldInterval 指的是当前执行的这个宏任务最长可执行时间
  * yieldInterval 初始值为 5ms，之后动态设置为一帧时间
- * 
+ *
  * 判断是否继续执行是在任务中判断的；schedule 只提供了 是否存在剩余时间 的 api
  * 任务是否执行 以及 是否发起一个宏任务 是两件事情：
  * 只要还存在未执行的任务时，总会发起一个新的宏任务，但任务是否会被执行取决于当前剩余时间
- * 
+ *
  * 时间切片：基于宏任务
- * 任务切片：基于一个所有任务可以共享的状态 + 时间切片，在本例中是 sum&i
+ * 任务切片：在任务实现中，基于时间切片，返回剩余任务，完成任务切片
  */
 const taskQueue = []
 // 一帧执行时间
@@ -18,11 +18,9 @@ let deadline = null
  * 一种所有任务可以共享的实现
  * ps：在 react render 中，fiber 是所有任务可以共享的
  */
-let sum = 0
-let i = 1
 
 const shouldYield = () => {
-  if(Date.now() >= deadline){
+  if (performance.now() >= deadline) {
     return true
   }
   return false
@@ -30,11 +28,11 @@ const shouldYield = () => {
 
 /**
  * 一个任务是否要出队取决于该任务的 callback 是否为空
- * 
+ *
  * 任务的 callback 的返回值（执行结果）是带有含义的：
  * - 返回值类型为 function，说明任务没有执行完
  * - 返回值类型不是 function，说明任务执行完毕
- * 
+ *
  * 任务的 callback 会在两个时机被更新：
  * - 开始执行任务时，置为 null
  * - 当次宏任务执行完毕时，更新为 callback 的返回值
@@ -42,10 +40,9 @@ const shouldYield = () => {
 const workLoop = () => {
   let nextTask = null
   let task = taskQueue[0]
-  try{
-    while(task !== null && !shouldYield()){
-
-      if(typeof task.callback !== 'function'){
+  try {
+    while (task !== null && !shouldYield()) {
+      if (typeof task.callback !== 'function') {
         taskQueue.shift()
         task = taskQueue[0]
       }
@@ -56,11 +53,11 @@ const workLoop = () => {
       // 应该只有这里需要防御
       nextTask = callback()
     }
-  }catch(e){
+  } catch (e) {
     throw e
-  }finally{
+  } finally {
     // 这种比 i < totalCount 要好很多，对 task 内部逻辑是透明的
-    if(typeof nextTask === 'function'){
+    if (typeof nextTask === 'function') {
       task.callback = nextTask
       return true
     }
@@ -70,20 +67,23 @@ const workLoop = () => {
 
 const performWorkUntilDeadline = () => {
   let hasMoreWork = true
-  try{
+  try {
     hasMoreWork = workLoop()
-  }catch(e){}finally{
-    if(!hasMoreWork){
+  } catch (e) {
+  } finally {
+    if (!hasMoreWork) {
+      // 移除 port1 onmessage 的事件监听，否则进程无法关闭（node 中），回调函数无法销毁
+      // port1.onmessage = null
       return
     }
     schedulePerformWorkUntilDeadline()
   }
 }
 
-const {port1, port2} = new MessageChannel()
+const { port1, port2 } = new MessageChannel()
 
 port1.onmessage = () => {
-  deadline = Date.now() + yieldInterval
+  deadline = performance.now() + yieldInterval
   // 执行可以中断的任务
   performWorkUntilDeadline()
 }
@@ -98,24 +98,33 @@ const init = () => {
 }
 
 const enququeTask = () => {
+  let i = 1
   const callback = () => {
     const totalCount = 2000000
-    for(; i < totalCount && !shouldYield(); i++){
-      sum += i
+    const start = performance.now()
+    let flag = false
+    for (; i < totalCount && !shouldYield(); i++) {
+      const index = i
+      Promise.resolve('resovled').then((data) => {
+        if (!flag) {
+          flag = true
+          // 确认微任务开始执行的时间 => 也就是当前宏任务执行完毕的时间
+          // 只有这个时间段等于 duration 才算符合预期
+          console.log(performance.now() - start)
+          console.log(data, index)
+        }
+
+        if (index === totalCount - 1) {
+          console.log(data, index)
+        }
+      })
     }
-  
-    if(i === totalCount){
-      i = 1
-      sum = 0
-      return
-    }
-  
     return callback
   }
   taskQueue.push({
     // 由于本例中任务执行顺序是严格按照插入顺序，所以不需要 id 来唯一标记任务
     // react 中由于添加了 任务优先级，所以执行顺序与插入任务顺序不同，需要有 id 来找到当前需要恢复的任务
-    id: taskQueue.length && taskQueue.at(-1).id + 1, 
+    id: taskQueue.length && taskQueue.at(-1).id + 1,
     callback
   })
 }
@@ -130,8 +139,8 @@ const step = () => {
   count++
   let now = performance.now()
   const gap = now - startTime
-  
-  if(gap > 1000){
+
+  if (gap > 1000) {
     yieldInterval = Math.floor(gap / count)
     count = 0
     startTime = now
@@ -140,7 +149,7 @@ const step = () => {
 }
 
 const clearRaf = () => {
-  if(!rafId){
+  if (!rafId) {
     return
   }
   cancelAnimationFrame(rafId)
@@ -158,7 +167,7 @@ const startRaf = () => {
 // 这里在隐藏页面时，取消 raf，在恢复展示时，重新注册 raf 回调
 document.addEventListener('visibilitychange', () => {
   const visibility = document.visibilityState
-  if(visibility === 'visible'){
+  if (visibility === 'visible') {
     startRaf()
     return
   }
@@ -172,7 +181,7 @@ const schedule = document.getElementById('schedule')
 
 schedule.onclick = () => {
   // init()
-  for(let i = 0; i < 10; i++){
+  for (let i = 0; i < 10; i++) {
     init()
   }
 }
